91f4670ea45c220fea24a480a31c64a1740c4591
23knightst813 <23knightst813@collyers.ac.uk>
2025-01-27 16:11:17 +0000

    Added Some Register Page Styling

--- flaskr/app/app.py
+++ flaskr/app/app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__, static_folder='../static')
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template('index.html')
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
